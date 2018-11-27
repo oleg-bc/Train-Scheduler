3f9106b1902214fb85d728cc831bb12d8ef53e4c
Oleg Hansen <oleg-ucb@outlook.com>
2018-11-26 18:06:12 -0800

    added firebase settup to the scriptjs file

---
+++ script.js
@@ -0,0 +1,10 @@
+ // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCumlK3Eb-6KunNpjOxdOGvbzPPt1rLlXE",
+    authDomain: "train-scheduler-fe7cc.firebaseapp.com",
+    databaseURL: "https://train-scheduler-fe7cc.firebaseio.com",
+    projectId: "train-scheduler-fe7cc",
+    storageBucket: "train-scheduler-fe7cc.appspot.com",
+    messagingSenderId: "486320349933"
+  };
+  firebase.initializeApp(config);
